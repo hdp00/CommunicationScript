781382d59d9c8edf41abad1db0bd4641e8764cbd
hdp <hdp00@126.com>
2025-06-24 16:58:53 +0800

    添加通讯接口

---
+++ CommunicationDevice/ICommunication.cs
@@ -0,0 +1,18 @@
+﻿//通讯类接口
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationDevice
+{
+    public interface ICommunication
+    {
+        bool Open();
+        bool Close();
+
+        bool Command(string command, out string response);
+        bool Command(byte[] command, out string[] response);
+    }
+}
